1eef7d7ee4729262e8add094224d6e0768c35139
courtneypattison <courtney@courtneypattison.com>
2018-09-04 14:10:33 -0700

    Added AngularFirestore2 to SleepService tests

--- src/app/sleep/shared/sleep.service.spec.ts
+++ src/app/sleep/shared/sleep.service.spec.ts
@@ -1,13 +1,22 @@
 import { TestBed, inject } from '@angular/core/testing';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+import { config } from '../../../testing/mock-config';
+
 import { SleepService } from './sleep.service';
 import { LoggerService } from '../../core/logger.service';
 
 import { MockLoggerService } from '../../../testing/mock-logger.service';
 
-describe('SleepLogService', () => {
+describe('SleepService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        AngularFireModule.initializeApp(config),
+        AngularFirestoreModule
+      ],
       providers: [
         SleepService,
         { provide: LoggerService, useClass: MockLoggerService }
---
+++ src/testing/mock-config.ts
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: 'AIzaSyBBSUUnxmB4J41tMcM7_aih3xiochpf0yE',
+  authDomain: 'sleep-logging.firebaseapp.com',
+  databaseURL: 'https://sleep-logging.firebaseio.com',
+  projectId: 'sleep-logging',
+  storageBucket: 'sleep-logging.appspot.com',
+  messagingSenderId: '893462679767'
+};
