4906ee6e6a61e5c2d0a1e35b8d5d2f85087e9191
courtneypattison <courtney@courtneypattison.com>
2018-09-18 12:59:44 -0700

    Added imports and providers to sleep form spec

--- src/app/sleep/sleep-form/sleep-form.component.spec.ts
+++ src/app/sleep/sleep-form/sleep-form.component.spec.ts
@@ -2,12 +2,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+import { config } from '../../../testing/mock-config';
+
 import { MatNativeDateModule } from '@angular/material/';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatSelectModule } from '@angular/material/select';
+
+import { SleepService } from '../shared/sleep.service';
+import { LoggerService } from '../../core/logger.service';
+
+import { MockLoggerService } from '../../../testing/mock-logger.service';
 
 import { SleepFormComponent } from './sleep-form.component';
 
@@ -21,12 +31,18 @@ describe('SleepFormComponent', () => {
         FormsModule,
         BrowserAnimationsModule,
         ReactiveFormsModule,
+        AngularFireModule.initializeApp(config),
+        AngularFirestoreModule,
         MatNativeDateModule,
         MatButtonModule,
         MatInputModule,
         MatFormFieldModule,
         MatDatepickerModule,
         MatSelectModule,
+      ],
+      providers: [
+        SleepService,
+        { provide: LoggerService, useClass: MockLoggerService }
       ],
       declarations: [SleepFormComponent]
     })
