e66d7233f2d5160371b1f6c05cf72c1c61987b1e
courtneypattison <courtney@courtneypattison.com>
2018-09-18 10:51:37 -0700

    Added save to firestore

--- src/app/sleep/sleep-form/sleep-form.component.ts
+++ src/app/sleep/sleep-form/sleep-form.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { DatePipe } from '@angular/common';
 
+import * as firebase from 'firebase/app';
+
 import { SleepState } from '../shared/sleep-state.model';
+import { SleepService } from '../shared/sleep.service';
 
 @Component({
   selector: 'sl-sleep-form',
@@ -12,20 +15,40 @@ import { SleepState } from '../shared/sleep-state.model';
 export class SleepFormComponent implements OnInit {
   datePipe = new DatePipe(navigator.language);
   sleepForm = this.formBuilder.group({
-    startDate: [this.datePipe.transform(new Date(), 'y-MM-dd'), Validators.required],
-    startTime: [this.datePipe.transform(new Date(), 'shortTime'), Validators.required],
-    startSleepState: [SleepState.Asleep.toString(), Validators.required]
+    startDate: [new Date(), Validators.required],
+    startTime: [this.datePipe.transform(new Date(), 'shortTime'), [
+      Validators.required,
+      Validators.pattern('(1[0-2]|0?[1-9]):([0-5][0-9]) ([AaPp][Mm])')
+    ]],
+    sleepState: ['', Validators.required]
   });
 
   sleepStates = Object.values(SleepState).filter(value => typeof value === 'string') as string[];
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private sleepService: SleepService) { }
 
   ngOnInit() {
   }
 
-  onSubmit() {
-    console.log(this.sleepForm.value);
+  private getStartHours(): number {
+    const numbers = this.sleepForm.value.startTime.split(':');
+    // Convert to 24 hour clock
+    return numbers[1].includes('P') || numbers[1].includes('p') ? Number(numbers[0]) + 12 : Number(numbers[0]);
+  }
+
+  private getStartMinutes(): number {
+    const startTime = this.sleepForm.value.startTime;
+    const colonIndex = startTime.search(':');
+    return Number(startTime.slice(colonIndex + 1, -3));
+  }
+
+  private getStartDateTime(): Date {
+    const startDate = new Date(this.sleepForm.value.startDate);
+    return new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate(), this.getStartHours(), this.getStartMinutes());
+  }
+
+  onSave() {
+    this.sleepService.add(this.getStartDateTime(), this.sleepForm.value.sleepState);
   }
 
 }
