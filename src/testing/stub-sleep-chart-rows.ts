7f0a082d4ffc4989f1885199af5ba0ba77c9c1af
courtneypattison <courtney@courtneypattison.com>
2018-11-26 10:04:18 -0800

    Fixed tests for date in chart

--- src/testing/stub-sleep-chart-rows.ts
+++ src/testing/stub-sleep-chart-rows.ts
@@ -1,8 +1,8 @@
 export const StubSleepChartRows = [
-  ['5/25/18', 'Fussing', new Date(0, 0, 0, 1, 25), new Date(0, 0, 0, 2, 25)],
-  ['5/25/18', 'Asleep', new Date(0, 0, 0, 2, 25), new Date(0, 0, 0, 24, 0)],
-  ['5/26/18', 'Asleep', new Date(0, 0, 0, 0, 0), new Date(0, 0, 0, 2, 25)],
-  ['5/26/18', 'Awake', new Date(0, 0, 0, 2, 25), new Date(0, 0, 0, 2, 30)],
-  ['5/29/18', 'Asleep', new Date(0, 0, 0, 4, 30), new Date(0, 0, 0, 24, 0)],
-  ['6/2/18', 'Asleep', new Date(0, 0, 0, 0, 0), new Date(0, 0, 0, 2, 30)],
+  ['5/25', 'Fussing', new Date(0, 0, 0, 1, 25), new Date(0, 0, 0, 2, 25)],
+  ['5/25', 'Asleep', new Date(0, 0, 0, 2, 25), new Date(0, 0, 0, 24, 0)],
+  ['5/26', 'Asleep', new Date(0, 0, 0, 0, 0), new Date(0, 0, 0, 2, 25)],
+  ['5/26', 'Awake', new Date(0, 0, 0, 2, 25), new Date(0, 0, 0, 2, 30)],
+  ['5/29', 'Asleep', new Date(0, 0, 0, 4, 30), new Date(0, 0, 0, 24, 0)],
+  ['6/2', 'Asleep', new Date(0, 0, 0, 0, 0), new Date(0, 0, 0, 2, 30)],
 ];
