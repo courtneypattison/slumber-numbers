287a2707bd74730132bb2aa734d4f9640d5fcb4f
courtneypattison <courtney@courtneypattison.com>
2018-11-26 18:43:15 -0800

    Fixed getStartHours 12 am/pm bug

--- src/app/sleep-time/sleep-time-form/sleep-time-form.component.ts
+++ src/app/sleep-time/sleep-time-form/sleep-time-form.component.ts
@@ -1,5 +1,5 @@
 import { DatePipe } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 
 import { State } from 'app/sleep-time/shared/state.model';
@@ -10,13 +10,13 @@ import { SleepTimeService } from 'app/sleep-time/shared/sleep-time.service';
   templateUrl: './sleep-time-form.component.html',
   styleUrls: ['./sleep-time-form.component.css']
 })
-export class SleepTimeFormComponent implements OnInit {
+export class SleepTimeFormComponent {
   datePipe = new DatePipe(navigator.language);
   sleepTimeForm = this.formBuilder.group({
     startDate: [new Date(), Validators.required],
     startTime: [this.datePipe.transform(new Date(), 'shortTime'), [
       Validators.required,
-      Validators.pattern('(1[0-2]|0?[1-9]):([0-5][0-9]) ([AaPp][Mm])')
+      Validators.pattern('(1[0-2]|0?[1-9]):([0-5][0-9]) *([AaPp][Mm])')
     ]],
     state: ['', Validators.required]
   });
@@ -25,27 +25,32 @@ export class SleepTimeFormComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder, private sleepService: SleepTimeService) { }
 
-  ngOnInit() {
-  }
-
   private getStartHours(): number {
-    const numbers = this.sleepTimeForm.value.startTime.split(':');
+    const startTime = this.sleepTimeForm.value.startTime;
+    const hours = Number(this.sleepTimeForm.value.startTime.split(':')[0]);
+
     // Convert to 24 hour clock
-    return numbers[1].includes('P') || numbers[1].includes('p') ? Number(numbers[0]) + 12 : Number(numbers[0]);
+    if (startTime.search(/P/i) !== -1) { // PM
+      return (hours === 12) ? 12 : hours + 12;
+    } else { // AM
+      return (hours === 12) ? 0 : hours;
+    }
   }
 
   private getStartMinutes(): number {
     const startTime = this.sleepTimeForm.value.startTime;
     const colonIndex = startTime.search(':');
+
     return Number(startTime.slice(colonIndex + 1, -3));
   }
 
   private getStartDateTime(): Date {
     const startDate = new Date(this.sleepTimeForm.value.startDate);
+
     return new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate(), this.getStartHours(), this.getStartMinutes());
   }
 
-  onsetSleepTime() {
+  onSetSleepTime() {
     this.sleepService.setSleepTime(this.getStartDateTime(), this.sleepTimeForm.value.state);
   }
 
