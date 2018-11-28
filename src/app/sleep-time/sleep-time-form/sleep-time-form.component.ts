54848fa9c09f476bb313b642a8375daf9166a889
courtneypattison <courtney@courtneypattison.com>
2018-11-28 09:51:01 -0800

    Changed time picker to mat-select

--- src/app/sleep-time/sleep-time-form/sleep-time-form.component.ts
+++ src/app/sleep-time/sleep-time-form/sleep-time-form.component.ts
@@ -12,46 +12,49 @@ import { SleepTimeService } from 'app/sleep-time/shared/sleep-time.service';
 })
 export class SleepTimeFormComponent {
   datePipe = new DatePipe(navigator.language);
+  currentDate = new Date();
   sleepTimeForm = this.formBuilder.group({
-    startDate: [new Date(), Validators.required],
-    startTime: [this.datePipe.transform(new Date(), 'shortTime'), [
-      Validators.required,
-      Validators.pattern('(1[0-2]|0?[1-9]):([0-5][0-9]) *([AaPp][Mm])')
-    ]],
+    startDate: [this.currentDate, Validators.required],
+    hour: [this.datePipe.transform(this.currentDate, 'h'), Validators.required],
+    minute: [this.datePipe.transform(this.currentDate, 'm'), Validators.required],
+    period: [this.datePipe.transform(this.currentDate, 'a'), Validators.required],
     state: ['', Validators.required]
   });
 
   states = Object.values(State).filter(value => typeof value === 'string') as string[];
+  hours = getNumbers(1, 12);
+  minutes = getNumbers(0, 59);
+  periods = ['AM', 'PM'];
 
   constructor(private formBuilder: FormBuilder, private sleepService: SleepTimeService) { }
 
-  private getStartHours(): number {
-    const startTime = this.sleepTimeForm.value.startTime;
-    const hours = Number(this.sleepTimeForm.value.startTime.split(':')[0]);
+  private getStartHour(): number {
+    const hour = this.sleepTimeForm.value.hour;
+    const period = this.sleepTimeForm.value.period;
 
     // Convert to 24 hour clock
-    if (startTime.search(/P/i) !== -1) { // PM
-      return (hours === 12) ? 12 : hours + 12;
-    } else { // AM
-      return (hours === 12) ? 0 : hours;
+    if (period === 'PM') {
+      return (hour === 12) ? 12 : hour + 12;
+    } else {
+      return (hour === 12) ? 0 : hour;
     }
-  }
-
-  private getStartMinutes(): number {
-    const startTime = this.sleepTimeForm.value.startTime;
-    const colonIndex = startTime.search(':');
-
-    return Number(startTime.slice(colonIndex + 1, -3));
   }
 
   private getStartDateTime(): Date {
     const startDate = new Date(this.sleepTimeForm.value.startDate);
+    const minute = this.sleepTimeForm.value.minute;
 
-    return new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate(), this.getStartHours(), this.getStartMinutes());
+    return new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate(), this.getStartHour(), minute);
   }
 
   onSetSleepTime() {
-    this.sleepService.setSleepTime(this.getStartDateTime(), this.sleepTimeForm.value.state);
+    const state = this.sleepTimeForm.value.state;
+
+    this.sleepService.setSleepTime(this.getStartDateTime(), state);
   }
 
+}
+
+function getNumbers(start: number, end: number): number[] {
+  return Array.from(Array(end).keys()).map((value) => value + start);
 }
