25ac8799376a11331aadae0d7dc083084387f8b3
courtneypattison <courtney@courtneypattison.com>
2018-11-29 12:31:48 -0800

    Fixed sleep time form hours bug

--- src/app/sleep-time/sleep-time-form/sleep-time-form.component.ts
+++ src/app/sleep-time/sleep-time-form/sleep-time-form.component.ts
@@ -22,7 +22,7 @@ export class SleepTimeFormComponent {
   });
 
   states = Object.values(State).filter(value => typeof value === 'string') as string[];
-  hours = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+  hours = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
   minutes = [
     '00', '01', '02', '03', '04', '05', '06', '07', '08', '09',
     '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
